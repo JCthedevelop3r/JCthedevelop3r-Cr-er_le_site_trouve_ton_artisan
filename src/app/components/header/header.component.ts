ccefa2f10c88829ab0a308c693c59b99c3b962b5
JIICE <minotjeanclaude60@gmail.com>
2024-07-21 17:52:35 +0100

    Add toggle functions and effects to the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  
+  ngOnInit(): void {
+    // Permet d'initialiser les styles display lors du chargement du composant
+    const navbarIcon = document.querySelector('.toggler-icon-container') as HTMLElement;
+    const blueCross = document.querySelector('.blue-cross-icon') as HTMLElement;
+
+    if (navbarIcon) {
+      navbarIcon.style.display = "block";
+    }
+    if (blueCross) {
+      blueCross.style.display = "none";
+    }
+  }
+
+  navbarToggler(): void {
+    const navbarIcon = document.querySelector('.toggler-icon-container') as HTMLElement;
+    const blueCross = document.querySelector('.blue-cross-icon') as HTMLElement;
+
+    if (navbarIcon && blueCross) {
+      if (navbarIcon.style.display === "block") {
+        navbarIcon.style.display = "none";
+        blueCross.style.display = "inline-block";
+      } else {
+        navbarIcon.style.display = "block";
+        blueCross.style.display = "none";
+      }
+    }
+  }
+
+  searchToggler(): void {
+    const searchInput = document.getElementById('app-search-input') as HTMLElement;
+
+    if(searchInput?.style.display === "none") {
+      searchInput.style.display ="block";
+    } else {
+      searchInput.style.display ="none";
+    }
+  }
 }
